ede47219b32e53c84ea49ca7a2e4749e14814ddb
Robin <r.m.p.linde@students.uu.nl>
2020-08-03 12:23:13 +0200

    Adjust performance preparation

--- routes/request_api.py
+++ routes/request_api.py
@@ -1,9 +1,10 @@
 import uuid
 from flask import jsonify, abort, request, Blueprint
 import os
-from planning_logic.icpcp_greedy_repair_cycle import
+import planning_logic.icpcp_greedy_repair_cycle as icpcp_greedy_repair
 import networkx as nx
 import random as rng
+import sys
 REQUEST_API = Blueprint('request_api', __name__)
 
 
@@ -12,7 +13,7 @@ def get_blueprint():
     return REQUEST_API
 
 
-def prepare_icpcp(dependencies, tasks, performance_model):
+def prepare_icpcp(dependencies, tasks, performance_model=None):
     G = nx.DiGraph()
     #add tasks to graph
     for i in range(0, len(tasks)):
@@ -36,11 +37,11 @@ def prepare_icpcp(dependencies, tasks, performance_model):
 
     number_of_nodes = G.number_of_nodes()
     t = 0
-    for line in performance_model:
+    for line in range(0, len(performance_model)):
         t += 1
         tstr = "time" + str(t)
         for inode in range(0, number_of_nodes):
-            G.node[inode][tstr] = performance_model[inode]
+            G.node[inode][tstr] = performance_model[line][inode]
 
     print(G.number_of_nodes())
     return G
@@ -56,9 +57,22 @@ def send_vm_configuration():
     data = request.get_json(force=True)
     dependencies = data['dependencies']
     tasks = data['tasks']
-    icpcp_params = data['icpcp-params']
+    icpcp_params = data['icpcp_params']
+    performance = price = icpcp_params['performance']
+    price = icpcp_params['price']
+    deadline = icpcp_params['deadline']
     # endpoint_parameters = data[]
-    graph = prepare_icpcp(dependencies, tasks)
+    graph = prepare_icpcp(dependencies, tasks, performance)
+    icpcp_greedy_repair.main(sys.argv[1:], command_line=False, graph=graph, prep_prices=price, prep_deadline=deadline)
+    print(icpcp_greedy_repair.number_of_nodes)
+    print(icpcp_greedy_repair.instances)
+
     # TODO: handle error codes
     return jsonify(data), 200
 
+
+if __name__ == '__main__':
+    print(os.getcwd())
+    icpcp_greedy_repair.main(sys.argv[1:])
+    print(icpcp_greedy_repair.number_of_nodes)
+    print(icpcp_greedy_repair.instances)
